fc3c058c51fbf9db6cdc33127c693b58af5e14d1
Xaspy <64539300+Xaspy@users.noreply.github.com>
2020-10-09 16:25:29 +0500

    Created chat-bot with keyboard

---
+++ src/main/java/com/phrases/Phrases.java
@@ -0,0 +1,44 @@
+package com.phrases;
+
+public class Phrases {
+    final String[] questions;
+    final String[] answers;
+    final String[] chooseGame;
+    final String readiness;
+    final String information;
+
+    public Phrases() {
+        questions = new String[] {"Что надо, хозяин?", "Чего хочешь?", "Дааа?"};
+        answers = new String[] {"Почему бы и нет", "Угу", "Хорошо", "Попробуем"};
+        chooseGame = new String[] {"Во что?", "Выбирай игру", "Выбери"};
+        readiness = "Готов вкалывать!";
+        information = "Я игровой многофункциональный раб.., ой, то есть бот.\n" +
+                "В данный момент ты можешь сыграть в:\n";
+        //Список игр сюда как-нибудь присобачить
+    }
+
+    public String getQuestion() {
+        return questions[getRandom(questions.length - 1)];
+    }
+
+    public String getAnswer() {
+        return answers[getRandom(answers.length - 1)];
+    }
+
+    public String getChooseGame() {
+        return chooseGame[getRandom(chooseGame.length - 1)];
+    }
+
+    public String getReadiness() {
+        return readiness;
+    }
+
+    public String getInfo() {
+        return information;
+    }
+
+    private int getRandom(int max)
+    {
+        return (int)(Math.random() * ++max);
+    }
+}
--- src/main/java/com/playgroundbot/PlaygroundBot.java
+++ src/main/java/com/playgroundbot/PlaygroundBot.java
@@ -1,36 +1,87 @@
 package com.playgroundbot;
 
+import com.phrases.Phrases;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.ArrayList;
+
 public class PlaygroundBot extends TelegramLongPollingBot {
-    /**
-     * Метод-обработчик поступающих сообщений.
-     * @param update объект, содержащий информацию о входящем сообщении
-     */
+    private long chatId;
+    private String lastMessage;
+    private ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+
     @Override
     public void onUpdateReceived(Update update) {
+        SendMessage sendMessage = new SendMessage().setChatId(update.getMessage().getChatId());
+        chatId = update.getMessage().getChatId();
+        String text = update.getMessage().getText();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
         try {
-            //проверяем есть ли сообщение и текстовое ли оно
-            if (update.hasMessage() && update.getMessage().hasText()) {
-                //Извлекаем объект входящего сообщения
-                Message inMessage = update.getMessage();
-                //Создаем исходящее сообщение
-                SendMessage outMessage = new SendMessage();
-                //Указываем в какой чат будем отправлять сообщение
-                //(в тот же чат, откуда пришло входящее сообщение)
-                outMessage.setChatId(inMessage.getChatId());
-                //Указываем текст сообщения
-                outMessage.setText(inMessage.getText());
-                //Отправляем сообщение
-                execute(outMessage);
-            }
+            sendMessage.setText(getMessage(text));
+            execute(sendMessage);
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
+        lastMessage = text;
+//        try {
+//            if (update.hasMessage() && update.getMessage().hasText()) {
+//                Message inMessage = update.getMessage();
+//                User user = inMessage.getFrom();
+//                SendMessage outMessage = new SendMessage();
+//                outMessage.setChatId(inMessage.getChatId());
+//                outMessage.setText("Привет, " + user.getFirstName() + '!');
+//                execute(outMessage);
+//            }
+//        } catch (TelegramApiException e) {
+//            e.printStackTrace();
+//        }
+    }
+
+    private String getMessage(String msg) {
+        Phrases phrases = new Phrases();
+        ArrayList<KeyboardRow> keyboard = new ArrayList<>();
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(true);
+
+        if (msg.equals("/start") || msg.equals("Начать")) {
+            keyboardFirstRow.add("Играть");
+            keyboardFirstRow.add("Информация");
+            keyboard.add(keyboardFirstRow);
+            replyKeyboardMarkup.setKeyboard(keyboard);
+            return phrases.getQuestion();
+        }
+        if (msg.equals("Играть") && lastMessage.equals("Начать")) {
+            keyboardFirstRow.add("Морской бой");
+            keyboard.add(keyboardFirstRow);
+            replyKeyboardMarkup.setKeyboard(keyboard);
+            return phrases.getChooseGame();
+        }
+        if (msg.equals("Информация") && lastMessage.equals("Начать")) {
+            keyboardFirstRow.add("Начать");
+            keyboard.add(keyboardFirstRow);
+            replyKeyboardMarkup.setKeyboard(keyboard);
+            return phrases.getInfo();
+        }
+        if (msg.equals("Морской бой") && lastMessage.equals("Играть")) {
+            keyboardFirstRow.add("Морской бой");
+            keyboard.add(keyboardFirstRow);
+            replyKeyboardMarkup.setKeyboard(keyboard);
+            return phrases.getAnswer();
+        }
+        keyboardFirstRow.add("Начать");
+        keyboard.add(keyboardFirstRow);
+        replyKeyboardMarkup.setKeyboard(keyboard);
+        return phrases.getReadiness();
     }
 
     @Override
