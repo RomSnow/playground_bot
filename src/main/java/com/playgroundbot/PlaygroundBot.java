468836e6ddbd2eef8f72c97cc86194bdf863f7e9
Xaspy <64539300+Xaspy@users.noreply.github.com>
2020-10-09 14:20:10 +0500

    Стандартный эхо-бот, найти - 'PlaygroundMasterBot'

---
+++ src/main/java/com/playgroundbot/PlaygroundBot.java
@@ -0,0 +1,45 @@
+package com.playgroundbot;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class PlaygroundBot extends TelegramLongPollingBot {
+    /**
+     * Метод-обработчик поступающих сообщений.
+     * @param update объект, содержащий информацию о входящем сообщении
+     */
+    @Override
+    public void onUpdateReceived(Update update) {
+        try {
+            //проверяем есть ли сообщение и текстовое ли оно
+            if (update.hasMessage() && update.getMessage().hasText()) {
+                //Извлекаем объект входящего сообщения
+                Message inMessage = update.getMessage();
+                //Создаем исходящее сообщение
+                SendMessage outMessage = new SendMessage();
+                //Указываем в какой чат будем отправлять сообщение
+                //(в тот же чат, откуда пришло входящее сообщение)
+                outMessage.setChatId(inMessage.getChatId());
+                //Указываем текст сообщения
+                outMessage.setText(inMessage.getText());
+                //Отправляем сообщение
+                execute(outMessage);
+            }
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "PlaygroundMasterBot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "1179723857:AAGNIjsk9XvwrC0mBg6-c4nti8AGuCZeBso";
+    }
+}
