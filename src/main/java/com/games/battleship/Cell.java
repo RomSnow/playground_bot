fc2ef2765801cc6bfe7ee549cc856ce93c8eceda
RomSnow <ivashkin.roma2001@yandex.ru>
2020-10-13 10:33:36 +0500

    Made Cell class

--- src/main/java/com/games/battleship/Cell.java
+++ src/main/java/com/games/battleship/Cell.java
@@ -1,8 +1,45 @@
 package com.games.battleship;
 
-public enum Cell {
-    Empty,
-    Ship,
+public class Cell {
+    private CellType type;
+    private Ship ship;
+
+    public Cell(){
+        this.type = CellType.Empty;
+    }
+
+    public CellType getType(){
+        return this.type;
+    }
+
+    public void setShip(Ship ship)
+    {
+        this.ship = ship;
+        this.type = CellType.Ship;
+    }
+
+    public void setMiss(){
+        this.type = CellType.Miss;
+    }
+
+    public void setHit(){
+        this.ship.makeHit();
+        this.type = CellType.Hit;
+    }
+
+    public boolean isEmpty(){
+        return this.type == CellType.Empty;
+    }
+
+    public boolean isShip(){
+        return this.type == CellType.Ship;
+    }
+
+}
+
+enum CellType{
     Miss,
-    Hit
+    Hit,
+    Ship,
+    Empty
 }
