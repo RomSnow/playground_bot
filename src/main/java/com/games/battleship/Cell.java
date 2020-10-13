a54ccfd7a27632c58f3a20c4d46dc775465cd2d8
RomSnow <ivashkin.roma2001@yandex.ru>
2020-10-13 21:35:24 +0500

    Corrected for tests

--- src/main/java/com/games/battleship/Cell.java
+++ src/main/java/com/games/battleship/Cell.java
@@ -4,40 +4,45 @@ public class Cell {
     private CellType type;
     private Ship ship;
 
-    public Cell(){
+    public Cell() {
         this.type = CellType.Empty;
     }
 
-    public CellType getType(){
+    public CellType getType() {
         return this.type;
     }
 
-    public void setShip(Ship ship)
-    {
+    public void setShip(Ship ship) throws Exception {
+        if (this.ship != null)
+            throw new Exception();
         this.ship = ship;
         this.type = CellType.Ship;
     }
 
-    public void setMiss(){
+    public void setMiss() {
         this.type = CellType.Miss;
     }
 
-    public void setHit(){
+    public void setEmpty() {
+        type = CellType.Empty;
+    }
+
+    public void setHit() {
         this.ship.makeHit();
         this.type = CellType.Hit;
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return this.type == CellType.Empty;
     }
 
-    public boolean isShip(){
+    public boolean isShip() {
         return this.type == CellType.Ship;
     }
 
 }
 
-enum CellType{
+enum CellType {
     Miss,
     Hit,
     Ship,
--- src/main/java/com/games/battleship/Field.java
+++ src/main/java/com/games/battleship/Field.java
@@ -1,45 +1,58 @@
 package com.games.battleship;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Field {
     private final Cell[][] field;
 
     public Field(int size) {
         field = new Cell[size][size];
-        for (int i = 0; i < 10; i++)
-            for (int j = 0; j < 10; j++)
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
                 field[i][j] = new Cell();
     }
 
-    public void setFieldOnPosition(Ship ship, Direction direction, Point startPoint) {
-        int startX = startPoint.getX();
-        int startY = startPoint.getY();
-
+    public boolean setFieldOnPosition(Ship ship, Direction direction, Point startPoint) {
         var currentPoint = startPoint;
+        var addedPoints = new ArrayList<Point>();
         for (int i = 0; i < ship.getSize(); i++) {
-            field[currentPoint.getX()][currentPoint.getY()].setShip(ship);
+            try {
+                field[currentPoint.getY()][currentPoint.getX()].setShip(ship);
+                addedPoints.add(currentPoint);
+            } catch (Exception e) {
+                rollbackShip(addedPoints);
+                return false;
+            }
+
             currentPoint = getNextPosition(currentPoint, direction);
         }
+
+        return true;
     }
 
     public boolean fire(Point point) {
         int x = point.getX();
         int y = point.getY();
 
-        if (field[x][y].isEmpty()) {
-            field[x][y].setMiss();
+        if (field[y][x].isEmpty()) {
+            field[y][x].setMiss();
             return false;
-        }
-        else if (field[x][y].isShip()) {
-            field[x][y].setHit();
+        } else if (field[y][x].isShip()) {
+            field[y][x].setHit();
             return true;
         }
 
         return false;
     }
 
-    private Point getNextPosition(Point currentPoint, Direction direction){
-        switch (direction){
+    public CellType getCellTypeOnPosition(Point point) {
+        return field[point.getY()][point.getX()].getType();
+    }
+
+    private Point getNextPosition(Point currentPoint, Direction direction) {
+        switch (direction) {
             case Down:
                 return new Point(currentPoint.getX(), currentPoint.getY() + 1);
             case Up:
@@ -50,6 +63,12 @@ public class Field {
                 return new Point(currentPoint.getX() + 1, currentPoint.getY());
             default:
                 return currentPoint;
+        }
+    }
+
+    private void rollbackShip(List<Point> pointsToRollback) {
+        for (Point point: pointsToRollback){
+            field[point.getY()][point.getX()].setEmpty();
         }
     }
 }
