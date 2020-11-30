c3b406bf6891ee09be8f1fd5cc72ff5dc2b5f57a
RomSnow <ivashkin.roma2001@yandex.ru>
2020-11-30 17:18:04 +0500

    Fix tests

--- src/test/java/com/games/battleship/BattleShipTests.java
+++ src/test/java/com/games/battleship/BattleShipTests.java
@@ -8,7 +8,7 @@ public class BattleShipTests {
 
     @Test
     public void testSetShip() {
-        var player = new Player(5);
+        var player = new Player(new GameParams(0, 0, 1, 1, 10));
         var field = player.getPlayerField();
 
         //creation test
@@ -24,28 +24,37 @@ public class BattleShipTests {
 
     @Test
     public void testDirectionsSetShip() {
-        var player = new Player(20);
+        var player = new Player(new GameParams(0,4,0,0,20));
         var field = player.getPlayerField();
 
-        player.setShip(new Point(0, 0), Direction.Down, 2);
-        assertEquals(CellType.Ship, field.getCellTypeOnPosition(new Point(0, 1)));
+        try {
+            player.setShip(new Point(0, 0), Direction.Down, ShipType.twoSize);
+            assertEquals(CellType.Ship, field.getCellTypeOnPosition(new Point(0, 1)));
 
-        player.setShip(new Point(19, 0), Direction.Left, 2);
-        assertEquals(CellType.Ship, field.getCellTypeOnPosition(new Point(18, 0)));
+            player.setShip(new Point(19, 0), Direction.Left, ShipType.twoSize);
+            assertEquals(CellType.Ship, field.getCellTypeOnPosition(new Point(18, 0)));
 
-        player.setShip(new Point(0, 19), Direction.Right, 2);
-        assertEquals(CellType.Ship, field.getCellTypeOnPosition(new Point(1, 19)));
+            player.setShip(new Point(0, 19), Direction.Right, ShipType.twoSize);
+            assertEquals(CellType.Ship, field.getCellTypeOnPosition(new Point(1, 19)));
 
-        player.setShip(new Point(19, 19), Direction.Up, 2);
-        assertEquals(CellType.Ship, field.getCellTypeOnPosition(new Point(19, 18)));
+            player.setShip(new Point(19, 19), Direction.Up, ShipType.twoSize);
+            assertEquals(CellType.Ship, field.getCellTypeOnPosition(new Point(19, 18)));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Test
     public void testFire() {
-        var player = new Player(5);
+        var player = new Player(new GameParams(1, 0, 0, 0, 5));
         var field = player.getPlayerField();
 
-        player.setShip(new Point(0, 0), Direction.Down, 3);
+        try {
+            player.setShip(new Point(0, 0), Direction.Down, ShipType.oneSize);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         for (var i = 0; i < 3; i++)
             field.fire(new Point(0, i));
 
@@ -58,7 +67,7 @@ public class BattleShipTests {
 
     @Test
     public void testSwitchPlayer() {
-        var game = new BattleshipGame(5,
+        var game = new BattleshipGame(new GameParams(0, 0,0,0, 5),
                 "first", "second");
         var currentField = game.getCurrentPlayerField("first");
         var newField = game.getEnemyField("second");
@@ -68,18 +77,31 @@ public class BattleShipTests {
 
     @Test
     public void testSetShipAndHit(){
-        var game = new BattleshipGame(5,
+        var game = new BattleshipGame(new GameParams(0, 0, 1,0, 5),
                 "first", "second");
         var zeroPoint = new Point(0, 0);
-        game.setShip("first",3, zeroPoint, Direction.Right);
-        game.makeHit("first", zeroPoint);
+        try {
+            game.setShip("first",3, zeroPoint, Direction.Right);
+            game.setShip("second", 3, zeroPoint, Direction.Right);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            game.makeHit("first", zeroPoint);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         assertSame(game.getCurrentPlayerField("first").
                 getCellTypeOnPosition(zeroPoint), CellType.Ship);
         assertSame(game.getEnemyField("first").getCellTypeOnPosition(zeroPoint),
                 CellType.Miss);
 
-        game.makeHit("second", zeroPoint);
+        try {
+            game.makeHit("second", zeroPoint);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         assertSame(game.getEnemyField("second").getCellTypeOnPosition(zeroPoint),
                 CellType.Hit);
@@ -87,11 +109,20 @@ public class BattleShipTests {
 
     @Test
     public void testIncorrectMove(){
-        var game = new BattleshipGame(5, "first", "second");
+        var game = new BattleshipGame(new GameParams(0, 0, 0, 0, 5),
+                "first", "second");
 
-        assertFalse(game.makeHit("first", new Point(6,6)));
-        assertFalse(game.setShip("first", 5, new Point(0, 0), Direction.Up));
-        assertFalse(game.setShip("first", 5, new Point(-1, -1), Direction.Down));
+        try {
+            assertFalse(game.makeHit("first", new Point(6,6)));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            assertFalse(game.setShip("first", 5, new Point(0, 0), Direction.Up));
+            assertFalse(game.setShip("first", 5, new Point(-1, -1), Direction.Down));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
